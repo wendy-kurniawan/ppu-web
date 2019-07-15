b883bd744b8ac170ccfe10f1ea2e6936588b6f55
HelliosDev <wiky.pedia@yahoo.com>
2019-07-15 20:26:09 +0700

    Initial Commit

---
+++ resources/views/partials/modal.blade.php
@@ -0,0 +1,27 @@
+<div class="modal fade" id="signUpModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="signUpModalLabel">Sign Up</h5>
+            </div>
+            <div class="modal-body">
+                <div class="container">
+                    <div class="row">
+                        <div class="col">
+                            <a href="#" class="btn-primary p-4">
+                                <span class="text-white font-weight-bold">Narasumber</span>
+                                <img src="img/icons/people_outline.svg" alt="choice icon">
+                            </a>
+                        </div>
+                        <div class="col">
+                            <a href="#" class="btn-success p-4">
+                                <span class="text-white font-weight-bold">Usaha (UMKM)</span>
+                                <img src="img/icons/location_city.svg" alt="choice icon">
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
