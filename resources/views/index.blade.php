fea48da8f3ce323818596eba653eaccd20d3ffce
HelliosDev <wiky.pedia@yahoo.com>
2019-07-15 20:25:39 +0700

    Re-Change with no keyword PUBLIC + Added class to navbar + Added Modal

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Pemerataan Pemberdayaan UMKM')
 
 @section('styles')
-    <link rel="stylesheet" href="{{ asset('public/css/index.css') }}" type="text/css">
+    <link rel="stylesheet" href="{{ asset('css/index.css') }}" type="text/css">
 @endsection
 
 @section('content')
@@ -13,12 +13,12 @@
             <!-- brand -->
             <a class="navbar-brand" href="#">PPU</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+                <span class="navbar-toggler-icon"></span>
             </button>
         
             <!-- Nav Items -->
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav ml-auto">
+                <ul class="navbar-nav ml-auto font-weight-bold">
                     <li class="nav-item">
                         <a class="nav-link" href="#services">Services</a>
                     </li>
@@ -26,10 +26,10 @@
                         <a class="nav-link" href="#about">About</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Sign In</a>
+                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#signInModal">Sign In</button>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Sign Up</a>
+                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#signUpModal">Sign Up</button>
                     </li>
                 </ul>
             </div>
@@ -46,7 +46,7 @@
                         <a href="#" class="btn-start">Get Started</a>
                     </div>
                     <div class="col-lg-6">
-                        <img src="{{ asset('public/img/illustrations/business.svg') }}" alt="Illustration Business">
+                        <img src="{{ asset('img/illustrations/business.svg') }}" alt="Illustration Business">
                     </div>
                 </div>
             </div>
@@ -68,7 +68,7 @@
                                 <h3 class="card-title">Cari Solusi</h3>
                             </div>
                             <div class="text-center">
-                                <img class="card-img-top" src="{{ asset('public/img/illustrations/search-solutions.svg') }}" alt="Card image cap">
+                                <img class="card-img-top" src="{{ asset('img/illustrations/search-solutions.svg') }}" alt="Card image cap">
                             </div>
                         </div>
                     </div>
@@ -78,7 +78,7 @@
                                 <h3 class="card-title">Tentukan Lokasi</h3>
                             </div>
                             <div class="text-center">
-                                <img class="card-img-top" src="{{ asset('public/img/illustrations/choose-locations.svg') }}" alt="Card image cap">
+                                <img class="card-img-top" src="{{ asset('img/illustrations/choose-locations.svg') }}" alt="Card image cap">
                             </div>
                         </div>
                     </div>
@@ -88,7 +88,7 @@
                                 <h3 class="card-title">Seminar</h3>
                             </div>
                             <div class="text-center">
-                                <img class="card-img-top" src="{{ asset('public/img/illustrations/seminars.svg') }}" alt="Card image cap">
+                                <img class="card-img-top" src="{{ asset('img/illustrations/seminars.svg') }}" alt="Card image cap">
                             </div>
                         </div>
                     </div>
@@ -106,7 +106,7 @@
             </div>
             <div class="row mb-5 justify-content-center">
                 <div class="col">
-                    <img src="{{ asset('public/img/illustrations/solutions.svg') }}" alt="Illustration Solutions">
+                    <img src="{{ asset('img/illustrations/solutions.svg') }}" alt="Illustration Solutions">
                 </div>
                 <div class="col">
                     <h3 class="font-weight-bold">Solusi Pemberdayaan UMKM</h3>
@@ -119,7 +119,7 @@
                     <p>Kami menghubungkan anda selaku pemilik usaha UMKM dengan narasumber dari Lembaga yang menyelenggarakan event untuk menyelesaikan permasalahan dari UMKM.</p>
                 </div>
                 <div class="col">
-                    <img src="{{ asset('public/img/illustrations/maps.svg') }}" alt="Illustration Solutions">
+                    <img src="{{ asset('img/illustrations/maps.svg') }}" alt="Illustration Solutions">
                 </div>
             </div>
         </div>
@@ -128,4 +128,6 @@
     <footer class="py-2">
         <p class="text-center text-white font-weight-bold">Copyright &copy; 2019 PPU</p>
     </footer>
+
+    @include('partials.modal')
 @endsection
