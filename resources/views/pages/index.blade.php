e2cbecbb97d500db06635b93c461e8b6f09b658b
HelliosDev <wiky.pedia@yahoo.com>
2019-07-25 21:13:21 +0700

    Added navigation in partials

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -3,46 +3,14 @@
 @section('title', 'Pemerataan Pemberdayaan UMKM')
 
 @section('styles')
+    <link rel="stylesheet" href="{{ asset('css/nav.css') }}" type="text/css">
     <link rel="stylesheet" href="{{ asset('css/index.css') }}" type="text/css">
 @endsection
 
 @section('content')
     <img src="img/illustrations/abstract-shape.svg" alt="abstract" class="abstract">
     <div class="container">
-        <nav class="navbar navbar-expand-lg navbar-light">
-            <!-- brand -->
-            <a class="navbar-brand" href="#">PPU</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-        
-            <!-- Nav Items -->
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav ml-auto font-weight-bold">
-                    <li class="nav-item">
-                        <a class="nav-link" href="#services">Services</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#about">About</a>
-                    </li>
-                    @auth
-                        <li class="nav-item">
-                            <a class="nav-link btn-primary" href="{{ route('src') }}">My Dashboard</a>
-                        </li>
-                    @endauth
-                    @guest
-                        <li class="nav-item">
-                            {{-- <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#signInModal">Sign In</button> --}}
-                            <a class="nav-link btn-primary" href="{{ route('login') }}">Sign In</a>
-                        </li>
-                        <li class="nav-item">
-                            {{-- <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#signUpModal">Sign Up</button> --}}
-                            <a class="nav-link btn-primary" href="{{ route('register') }}">Sign Up</a>
-                        </li>
-                    @endguest
-                </ul>
-            </div>
-        </nav>
+        @include('partials.navigation')
     </div>
 
     <section id="banner">
