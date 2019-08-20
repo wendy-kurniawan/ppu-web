4968a397734c53b8247767fe3a0aa326aec8d607
Piter_Johan <piterjohan486@gmail.com>
2019-08-20 14:01:01 +0700

    Feature Search with Ajax and have time left on footer card

--- public/myjs/ajaxSearch.js
+++ public/myjs/ajaxSearch.js
@@ -12,14 +12,26 @@ $(document).ready(function () {
             url: $(this).data("url") + "/" + this.value,
 
             success: function (dataResult) {
-                let A = Date.now();
-                let B = A.toString();
-                console.log(B);
-
                 console.log(dataResult);
                 let domHtml = ``;
-                let domFunc = ``;
                 dataResult.forEach(function (itemEvent) {
+                    /*date INDO */
+                    let current_datetime = new Date()
+                    let currentFormattedDate = current_datetime.getFullYear() + "-" + ("0" + (+current_datetime.getMonth() + 1)).slice(-2) + "-" + current_datetime.getDate()
+                    /*Get date TGLMULAI */
+                    let getTGLMULAI = itemEvent.TGLMULAI;
+                    rvcvgetTGLMULAI = getTGLMULAI.split(' ')[0];
+                    /* substring first*/
+                    let subTGLMULAI = rvcvgetTGLMULAI.substring(5, 7); //monthTGLMULAI
+                    let subDateNow = currentFormattedDate.substring(5, 7); //monthNow
+                    /*count monthTglMulai - monthDatenow*/
+                    let sumMonth = Math.abs(subTGLMULAI - subDateNow);
+                    // console.log(subTGLMULAI, subDateNow, sumMonth);
+                    /*count dayTglMulai - dayDatenow*/
+                    let dayTGLMULAI = rvcvgetTGLMULAI.substring(8, 10); //dayTGLMULAI
+                    let dayDateNow = currentFormattedDate.substring(8, 10); //dayNow
+                    let sumDay = Math.abs(dayTGLMULAI - dayDateNow);
+
                     domHtml += `
                     <div class="card mx-2 mb-3" style="width: 22rem; box-shadow: 0 10px 10px rgba(0, 0, 0, 0.25)">
                         <p class="card-tag text-white font-weight-bold">${itemEvent.JKKEGIATAN}</p>
@@ -34,7 +46,29 @@ $(document).ready(function () {
                                 <h6 class="font-weight-bold">Lokasi</h6>
                                 <p>${itemEvent.LOKASI}</p>
                             </div>
-                        </div>`;
+                        </div>
+                        <div class="card-footer d-flex justify-content-between align-items-center">
+                            ${rvcvgetTGLMULAI < currentFormattedDate ?
+                                `<span class="font-weight-bold">Selesai</span>
+                                <span class="disable-links">
+                                    <button type="#" class="btn btn-danger" disabled>Pendaftar ditutup</button>   
+                                </span>` : ''}
+                            ${subDateNow == subTGLMULAI ? 
+                                `<span class="font-weight-bold">
+                                Tersisa ${sumDay} Hari
+                                </span>
+                                <a href="#" class="btn btn-primary">Daftar</a>`  
+                                : ""}
+                            ${rvcvgetTGLMULAI > currentFormattedDate ? 
+                                `<span class="font-weight-bold">
+                                Tersisa ${sumMonth} Bulan 
+                                ${sumDay}Hari
+                            </span>
+                            <a href="#" class="btn btn-primary">Daftar</a>`
+                                : ""}
+                        </div>
+                    </div>
+                    `;
                 })
                 $("#dataResultEvent").html(domHtml)
             }
